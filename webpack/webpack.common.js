040c0c75547b741e23a4ab8c45b8c1ffd92d8688
rongping.lrp@alibaba-inc.com <rongping.lrp@alibaba-inc.com>
2021-01-01 11:00:36 +0800

    ajust folder

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -7,24 +7,37 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
   entry: {
     background: Path.resolve(__dirname, '../src/js/background.ts'),
-    pupop: Path.resolve(__dirname, '../src/js/popup.ts'),
+    popup: Path.resolve(__dirname, '../src/js/popup.ts'),
     content: Path.resolve(__dirname, '../src/js/content.ts')
   },
   node:{ fs: 'empty' },
   output: {
-    path: Path.join(__dirname, '../extension/dest'),
-    filename: '[name].js'
+    path: Path.join(__dirname, '../extension'),
+    filename: 'js/[name].js'
   },
   optimization: {
     splitChunks: false
   },
   plugins: [
-    new CleanWebpackPlugin(['dest'], { root: Path.resolve(__dirname, '../extension') }),
+    new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
-      { from: Path.resolve(__dirname, '../public'), to: 'public' }
+      { from: Path.resolve(__dirname, '../src/public'), to: 'public' }
+    ]),
+    new CopyWebpackPlugin([
+      { from: Path.resolve(__dirname, '../src/manifest.json'), to: 'manifest.json' }
+    ]),
+    new CopyWebpackPlugin([
+      { from: Path.resolve(__dirname, '../src/imgs'), to: 'imgs' }
     ]),
     new HtmlWebpackPlugin({
-      template: Path.resolve(__dirname, '../src/index.html')
+      template: Path.resolve(__dirname, '../src/popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup']
+    }),
+    new HtmlWebpackPlugin({
+      template: Path.resolve(__dirname, '../src/background.html'),
+      filename: 'background.html',
+      chunks: ['background']
     }),
     new MiniCssExtractPlugin({
       filename: '[name].css'
